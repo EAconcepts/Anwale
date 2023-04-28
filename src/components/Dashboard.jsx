5bf2274c6607c3bc03b88b65896e6dd18936c13b
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-04-28 21:23:17 +0100

    modified:   src/components/Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLessThan, faUserCircle, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faLessThan, faUserCircle, faGear, faList, faCommenting, faChartSimple, faBolt, faWallet, faNairaSign, faPhoneVolume,  } from '@fortawesome/free-solid-svg-icons'
 
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Login from './auth/Login';
 import StaticFooter from './StaticFooter';
+import { faBell, faCircleQuestion, faGem, faUser } from '@fortawesome/free-regular-svg-icons';
 
 
 
@@ -29,12 +30,8 @@ const Dashboard = ({isLoggedIn, setIsLoggedIn, formValues, userValues,currentUse
     }
 
     return(
-        <div>
-            {/* <Navbar isLoggedIn={isLoggedIn} /> */}
-            {/* {!isLoggedIn &&(
-                <Login/>
-            )} */}
-            <nav className='flex justify-between gap-3 px-3 py-2 bg-slate-100'>
+        <div className='h-screen bg-slate-200'>
+            <nav className='flex justify-between gap-3 px-4 py-2 bg-slate-100'>
                 <div className='flex gap-3'>
                     <button onClick={handleBackIcon}>
                         <FontAwesomeIcon icon={faLessThan} className='text-xl mr-2' /> 
@@ -42,11 +39,67 @@ const Dashboard = ({isLoggedIn, setIsLoggedIn, formValues, userValues,currentUse
                     <FontAwesomeIcon icon={faUserCircle} className='text-xl text-slate-900'/>
                     <span className='text-xs'>{currentUser} </span>
                 </div>
-                
-               <button className='text-xs'>
-                SETTINGS <FontAwesomeIcon icon={faGear}/>
-               </button>
+                <button className='text-xs'>
+                    SETTINGS <FontAwesomeIcon icon={faGear}/>
+                </button>
             </nav>
+            <div className=''>
+                <div className='flex gap-1'>
+                    <div className='flex flex-col'>
+                        <div className='flex flex-col bg-white  rounded'>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faList}/> <span className='text-base'>My adverts</span>
+                            </NavLink>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faCommenting}/> <span className='text-base'>Feedback</span>
+                            </NavLink>
+                            <NavLink className='p-3'>
+                                <FontAwesomeIcon icon={faChartSimple}/> <span className='text-base'>Perfomance</span>
+                            </NavLink>
+                        </div>
+                        <div className='flex flex-col mt-2 bg-white rounded'>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faBolt}/> <span className='text-base'>PRO Sales</span>
+                            </NavLink>
+                            <NavLink className=' border-b-[0.2px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faGem}/> <span className='text-base'>Premium Services</span>
+                            </NavLink>
+                            <NavLink className='p-3'>
+                                <div className='flex gap-2'>
+                                    <FontAwesomeIcon icon={faWallet}/> 
+                                    <div className='flex flex-col -mt-2'>
+                                        <p className='text-xs'><FontAwesomeIcon icon={faNairaSign} className='text-[10px]'/> 0</p>
+                                        <p className=' text-[10px] text-gray-400'>MY BALANCE</p>
+                                    </div>
+                                </div>
+                            
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div className='flex flex-col bg-white rounded'>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faUser}/> <span className='text-base'>Followers</span>
+                            </NavLink>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faBell}/> <span className='text-base'>Notifications</span>
+                            </NavLink>
+                        
+                        </div>
+                        <div className='flex flex-col mt-2 bg-white rounded'>
+                            <NavLink className=' border-b-[0.5px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faPhoneVolume}/> <span className='text-base'>Manager's Call</span>
+                            </NavLink>
+                            <NavLink className=' border-b-[0.2px] border-b-slate-200 p-3'>
+                                <FontAwesomeIcon icon={faCircleQuestion}/> <span className='text-base'>FAQ</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+           
             
 
             <StaticFooter/>
